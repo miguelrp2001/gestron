09e0b5425e6b0de31128a5c9cf20aa06725c8e2f
Miguel Román Párraga <mromparb@g.educaand.es>
2022-06-18 18:20:53 +0200

    Update constants

--- src/app/services/config.constants.ts
+++ src/app/services/config.constants.ts
@@ -1,4 +1,5 @@
 // export const APIIP = "127.0.0.1";
 export const APIIP = "192.168.1.251";
-export const APIURL = "http://" + APIIP + ":8000"
+export const PORT = "8000";
+export const APIURL = "http://" + APIIP + ":" + PORT;
 
